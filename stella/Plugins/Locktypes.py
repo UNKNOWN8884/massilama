5a70143c803562f56c9dbaf17fe3f0d7d1058e83
Unkown <97676159+UNKNOWN8884@users.noreply.github.com>
2022-02-04 17:04:43 +0530

    Create Locktypes.py

---
+++ stella/Plugins/Locktypes.py
@@ -0,0 +1,33 @@
+from Stella import StellaCli
+from Stella.helper import custom_filter
+from Stella.helper.chat_status import isUserAdmin
+from Stella.helper.disable import disable
+
+from . import lock_map
+
+
+@StellaCli.on_message(custom_filter.command(commands='locktypes', disable=True))
+@disable
+async def locktypes(client, message):
+    
+    LOCKS_LIST = lock_map.LocksMap.list()
+
+    text = "The available locktypes are:\n"
+    for lock in LOCKS_LIST:
+        text += f"- {lock}\n"
+    
+    await message.reply(
+        text,
+        quote=True
+    )
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
