475dfc3e9e484950e1fe179193089ebb811acd89
Unkown <97676159+UNKNOWN8884@users.noreply.github.com>
2022-02-09 17:46:12 +0530

    Update url_shortner.py

--- plugins/josprojects/url_shortner.py
+++ plugins/josprojects/url_shortner.py
@@ -27,7 +27,7 @@ GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9
 
 reply_markup = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton(text='join projects channel', url='https://telegram.me/josprojects')
+        InlineKeyboardButton(text='join projects channel', url='https://telegram.me/Mksupport1')
         ]]
     )
 
