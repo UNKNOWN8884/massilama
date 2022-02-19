704708e6ec3721854b1ded53cc49e1bb51e2362d
Unkown <97676159+UNKNOWN8884@users.noreply.github.com>
2022-02-19 09:20:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -522,7 +522,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-        elif query.data == "N-Mᴏᴅᴇ":
+        elif query.data == "Magan":
         buttons = [[
             InlineKeyboardButton('', callback_data='Magan')
         ]]
