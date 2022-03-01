04ea08408b0413354b06e806f3007bf519b74a96
Unkown <97676159+UNKNOWN8884@users.noreply.github.com>
2022-03-01 07:08:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/josprojects/tgmoviebot.git /tgmoviebot
-cd /tgmoviebot
+git clone -b master https://github.com/UNKNOWN8884/massilama.git /massilama
+cd /massilama
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
