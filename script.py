d1ccd3b17785400e4f0e3108c64984d10b640b09
Unkown <97676159+UNKNOWN8884@users.noreply.github.com>
2022-04-14 17:23:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,6 +5,8 @@ class Script(object):
 
 <b>😀Hɪ Dᴜᴅᴇ Jᴜsᴛ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ. I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇs ᴀɴᴅ Mᴏʀᴇ Iɴғᴏ Cʟɪᴄᴋ Mᴇ Tʜᴇ Hᴇʟᴘ 🎐</b>"""
     
+    C_TXT = """hey"""
+
     B_TXT = """hey"""
 
     A_TXT = """hey"""
