f826dcefddc75c92488bdc32cf358f65289e7642
Tanujairam <88611197+Tanujairam123@users.noreply.github.com>
2022-04-14 19:55:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -871,7 +871,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=reply_markup,
             parse_mode='html'
-        
+        ) 
     elif query.data == "s":
         buttons = [[
         InlineKeyboardButton('Zᴏᴍʙɪᴇs', callback_data='zombies'),
