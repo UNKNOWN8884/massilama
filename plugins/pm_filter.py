b5719f4bf186e2cffd1038ae5b6b83512aa69e40
Unkown <97676159+UNKNOWN8884@users.noreply.github.com>
2022-04-14 19:19:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -874,25 +874,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         
     
           
-    elif query.data == "s":
-        buttons = [[
-        InlineKeyboardButton('Zᴏᴍʙɪᴇs', callback_data='zombies'),
-        InlineKeyboardButton('ᴘᴏᴋᴇᴍᴏɴ ɪɴғᴏ', callback_data='pok'),
-        InlineKeyboardButton('ɪɴғᴏ', callback_data='in'),
-        ],[
-        InlineKeyboardButton('sᴛɪᴄᴋᴇʀ', callback_data='sti'),
-        InlineKeyboardButton('ғᴇᴅᴇʀᴀᴛɪᴏɴ', callback_data='fed'),
-        InlineKeyboardButton('ɪɴsᴜʟᴛs', callback_data='ins'), 
-        ],[
-        InlineKeyboardButton('ᴛʜᴇ ᴇɴᴅ ɢᴏ ʙᴀᴄᴋ💕',   callback_data='next'),
-      ]]  
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await query.message.edit_text(
-            text=Script.S_TXT,
-            disable_web_page_preview=True,
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
+    
     elif query.data == "ins":
         buttons = [[
             InlineKeyboardButton('« Back', callback_data='s')
